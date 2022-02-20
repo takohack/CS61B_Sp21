8f96b50915e3560a00813b09ead16d40ae27fab4
lilongyu <lilongyu@ruijie.com.cn>
2022-02-20 14:08:38 +0800

    finish lab4

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,22 +3,23 @@
  */
 public class Collatz {
 
-    static int num = 6;
-
     /** Buggy implementation of nextNumber! */
-    public static void nextNumber(int n) {
-        if(n == Collatz.num)
-            return;
-        for(int i =0;i<n;i++){
-            System.out.print('*');
+    public static int nextNumber(int n) {
+        if (n %2 == 0) {
+            return n/2;
+        } else {
+            return n * 3 + 1;
         }
-        System.out.println();
-        nextNumber(n+1);
-        return;
     }
 
     public static void main(String[] args) {
-       nextNumber(1);
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
     }
 }
 
